36cc2a8178a92a62ac866a5398cd1884087fd097
vvvsssvs <96478826+vvvsssvs@users.noreply.github.com>
2022-10-13 22:48:25 +0530

    adding new class

---
+++ QA/scanner.java
@@ -0,0 +1,14 @@
+package QA;
+
+import java.util.Scanner;
+
+public class scanner {
+	public static void main(String[] args) {
+		Scanner obj1 = new Scanner(System.in);
+		System.out.println("Enter your Name");
+		String name = obj1.nextLine();
+		System.out.println("My Name is: " + name);
+		
+	}
+
+}
